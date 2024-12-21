b82bcdf2dc2e2e157246a8d3562d2a8e3a0fc7c0
Cakgemblung <151426649+Cakgemblung@users.noreply.github.com>
2024-12-21 20:43:12 +0700

    Update crowdfundRoutes.ts

--- src/routes/crowdfundRoutes.ts
+++ src/routes/crowdfundRoutes.ts
@@ -1,26 +1,18 @@
-const express = require('express');
+import express from 'express';
+import {
+    getAllCrowdfunds,
+    getCrowdfund,
+    createCrowdfund,
+    updateCrowdfund,
+    deleteCrowdfund,
+} from '../controllers/crowdfundController';
+
 const router = express.Router();
-const crowdfundController = require('../controllers/crowdfundController');
 
-// Route untuk Admin Landing Page (Melihat semua Crowdfund)
-router.get('/', crowdfundController.getCrowdfunds);
+router.get('/', getAllCrowdfunds);
+router.get('/:id', getCrowdfund);
+router.post('/create', createCrowdfund);
+router.put('/:id/edit', updateCrowdfund);
+router.delete('/:id', deleteCrowdfund);
 
-// Route untuk melihat Detail Crowdfund
-router.get('/:id', crowdfundController.getCrowdfundById);
-
-// Route untuk Edit Crowdfund
-router.put('/:id/edit', crowdfundController.updateCrowdfund);
-
-// Route untuk membuat Crowdfund baru
-router.post('/create', crowdfundController.createCrowdfund);
-
-// Route untuk menghapus Crowdfund
-router.delete('/:id', crowdfundController.deleteCrowdfund);
-
-// Route untuk menambahkan komentar pada crowdfund
-router.post('/:id/comments', crowdfundController.addComment);
-
-// Route untuk menghapus komentar pada crowdfund
-router.delete('/:id/comments/:commentId', crowdfundController.deleteComment);
-
-module.exports = router;
+export default router;
